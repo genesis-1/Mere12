3f5c09bb50c9fe55c30b9864189b02ba334f36cf
patrick ntwari <patrick_ntwari@live.com>
2019-11-21 19:37:13 +0200

    -Added Product Type controller. - generated the Crud Views and ActionMethods - Spacefied the Route of ProductType in NavBar.

---
+++ Mere12/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mere12.Data;
+using Mere12.Models;
+
+namespace Mere12.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/ProductTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.productTypes.ToListAsync());
+        }
+
+        // GET: Admin/ProductTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productTypes = await _context.productTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productTypes == null)
+            {
+                return NotFound();
+            }
+
+            return View(productTypes);
+        }
+
+        // GET: Admin/ProductTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ProductTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productTypes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+
+        // GET: Admin/ProductTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productTypes = await _context.productTypes.FindAsync(id);
+            if (productTypes == null)
+            {
+                return NotFound();
+            }
+            return View(productTypes);
+        }
+
+        // POST: Admin/ProductTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] ProductTypes productTypes)
+        {
+            if (id != productTypes.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productTypes);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductTypesExists(productTypes.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+
+        // GET: Admin/ProductTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productTypes = await _context.productTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productTypes == null)
+            {
+                return NotFound();
+            }
+
+            return View(productTypes);
+        }
+
+        // POST: Admin/ProductTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productTypes = await _context.productTypes.FindAsync(id);
+            _context.productTypes.Remove(productTypes);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductTypesExists(int id)
+        {
+            return _context.productTypes.Any(e => e.Id == id);
+        }
+    }
+}
