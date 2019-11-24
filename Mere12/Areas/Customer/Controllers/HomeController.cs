f6d77a283da452ed502e6873d6e095b9178d611a
patrick ntwari <patrick_ntwari@live.com>
2019-11-24 17:56:36 +0200

    -Added index Action Method.

--- Mere12/Areas/Customer/Controllers/HomeController.cs
+++ Mere12/Areas/Customer/Controllers/HomeController.cs
@@ -5,15 +5,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Mere12.Models;
+using Mere12.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mere12.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var productList = await _context.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags).ToListAsync();
+            return View(productList);
         }
 
         public IActionResult Privacy()
