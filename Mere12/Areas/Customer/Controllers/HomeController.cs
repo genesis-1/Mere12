45665a230d673d1c57cf368a8fd4d35f0eb3f069
patrick ntwari <patrick_ntwari@live.com>
2019-11-28 11:47:21 +0200

    Added DetailPost in HomeController.

--- Mere12/Areas/Customer/Controllers/HomeController.cs
+++ Mere12/Areas/Customer/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Mere12.Models;
 using Mere12.Data;
 using Microsoft.EntityFrameworkCore;
+using Mere12.Extensions;
 
 namespace Mere12.Controllers
 {
@@ -30,6 +31,21 @@ namespace Mere12.Controllers
             var product = await _context.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags).Where(m => m.Id == id).FirstOrDefaultAsync();
 
             return View(product);
+        }
+
+        [HttpPost,ActionName("Details")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DetailsPost(int id)
+        {
+             List<int> listShoppingCart = HttpContext.Session.Get<List<int>>("ssShopingCart");
+            if(listShoppingCart == null)
+            {
+                listShoppingCart = new List<int>();
+            }
+            listShoppingCart.Add(id);
+            HttpContext.Session.Set("ssShopingCart", listShoppingCart);
+            //redirect to Action index,inside home controller and Customer area
+            return RedirectToAction("Index", "Home", new { area = "Customer" });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
